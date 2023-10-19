808b401e4bfe6125799e9d97b18ca8bca96211a7
Lina Urdaneta <pixelada@gmail.com>
2023-10-18 21:59:51 -0700

    New side menu

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,20 +2,20 @@
 	import MenuIcon from "$lib/MenuIcon.svelte";
 	import "../app.css";
 	import SideMenu from "$lib/SideMenu.svelte";
+	import SideMenuTwo from "$lib/SideMenuTwo.svelte";
 </script>
 
-<main class="xl:container xl:mx-auto h-screen lg:h-fit lg:aspect-video flex overflow-hidden rounded-lg ring-1 ring-slate-900/10">
+<main class="xl:container xl:mx-auto h-full lg:h-fit lg:aspect-video flex overflow-hidden rounded-lg ring-1 ring-slate-900/10">
 	<div class="flex relative w-full">
 		<input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
 		<div class=" flex w-full relative justify-between">
-			<!-- Menu Icon -->
 			<MenuIcon />
 
 			<slot />
 		</div>
-		<div class="drawer-side absolute z-[2] overflow-hidden">
+		<div class="drawer-side absolute z-[2] overflow-hidden h-full">
 			<label for="my-drawer-3" aria-label="close sidebar" class="drawer-overlay" />
-			<SideMenu />
+			<SideMenuTwo />
 		</div>
 	</div>
 </main>
